ded6666b9927bad28a27f5b2447e6ac0716eac6e
coco1pops@yahoo.com <Indeeref4>
2025-08-14 10:03:33 +0100

    Changed database and cloud run

--- production/run_deploy.sh
+++ production/run_deploy.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ -f ~/tag.sh ]; then
+  source tag.sh
+else
+  TAG=1
+fi
+printf -v PTAG "%05d" "$TAG"
+
 # Exit immediately if a .env file is not found
 if [ ! -f .env ]; then
     echo "Error: .env file not found in the current directory."
@@ -30,13 +37,18 @@ BUILD_ARGS="${BUILD_ARGS} --set-env-vars FLASK_DB_NAME=${FLASK_DB_NAME}"
 BUILD_ARGS="${BUILD_ARGS} --set-env-vars ENVIRONMENT=${ENVIRONMENT}" 
 BUILD_ARGS="${BUILD_ARGS} --set-env-vars CLOUD_SQL_CONNECTION_NAME=${CLOUD_SQL_CONNECTION_NAME}" 
 
-echo "Starting gcloud beta code dev with build arguments:"
+echo "Starting deployment with build arguments:"
 echo $BUILD_ARGS
-#gcloud beta code dev --source=. $BUILD_ARGS
+
 gcloud run deploy editor \
-  --image europe-west4-docker.pkg.dev/big-pact-460610-j7/cloud-run-source-deploy/editor:123456 \
+  --image europe-west4-docker.pkg.dev/big-pact-460610-j7/cloud-run-source-deploy/editorapp:$PTAG \
   --platform managed \
   --region europe-west4 \
   --allow-unauthenticated \
-  --add-cloudsql-instances big-pact-460610-j7:europe-west4:editor\
+  --add-cloudsql-instances big-pact-460610-j7:europe-west4:editordb \
+  --service-account cloud-sql@big-pact-460610-j7.iam.gserviceaccount.com \
   $BUILD_ARGS
+
+echo "Updating TAG"
+TAG=$((TAG+1))
+echo "TAG=$TAG">~/tag.sh
